5aa758b4b87cc934d5ab5cb9f0c39e6e7f363d75
Julye-sama <40432071+Julye-sama@users.noreply.github.com>
2018-07-01 17:15:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ var bot = new Discord.Client({autoReconnect: true});
 });
 
 bot.on("guildMemberAdd",function(member) {
-    member.guild.channels.find("name","general").sendMessage(member.toString() + " welcome! ≧◡≦ take a seat and grab some cookies ~", ({files: ["./welcomefile/welcome.gif"]})).catch(console.error);
+    var image = ["./welcomefile/1.gif", "./welcomefile/2.gif", "./welcomefile/3.gif"];
+    var rand = Math.floor(Math.random() * image.length);
+    var randomImage = image[rand];
+    member.guild.channels.find("name","general").sendMessage(member.toString() + " welcome! ≧◡≦ take a seat and grab some cookies ~", ({files: [image[rand]]}));
+    console.log(randomImage)
 
     member.addRole(member.guild.roles.find("name","loid")).catch(console.error);
 
